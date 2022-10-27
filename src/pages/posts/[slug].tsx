59c5ec5187fadf6d6ef4320887c3ff8a27570ad1
Marcos Antonio <totonhorock@gmail.com>
2022-10-27 18:01:04 -0300

    Update [slug].tsx

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -46,7 +46,7 @@ export const getServerSideProps: GetServerSideProps = async ({
   if (!session?.activeSubscription) {
     return {
       redirect: {
-        destination: `/preview/posts/${slug}`,
+        destination: `/`,
         permanent: false,
       },
     };
